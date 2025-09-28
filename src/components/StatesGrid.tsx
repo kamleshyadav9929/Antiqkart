85e715ba3cfb98faea60c2468bca18ed00d0866e
kamleshyadav9929 <9929raoyadav6@gmail.com>
2025-09-28 20:05:54 +0530

    minor changes

--- src/components/StatesGrid.tsx
+++ src/components/StatesGrid.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { supabase } from "../../src/lib/supabaseClient";
-import { ArrowRight } from "lucide-react";
 
 interface State {
   id: string;
